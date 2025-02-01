e448ec0cc26785f0d74ab920afee13f93f4f3344
SanketLV <sanketlakhani80@gmail.com>
2025-02-01 20:19:52 +0530

    dashboard page added and chat CRUD functionality added

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import NavigationProvider from "@/lib/NavigationProvider";
+import { Authenticated } from "convex/react";
+import React from "react";
+
+const dashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <NavigationProvider>
+      <div className="flex h-screen">
+        <Authenticated>
+          <Sidebar />
+        </Authenticated>
+        <div className="flex-1">
+          <Header />
+          <main>{children}</main>
+        </div>
+      </div>
+    </NavigationProvider>
+  );
+};
+
+export default dashboardLayout;
